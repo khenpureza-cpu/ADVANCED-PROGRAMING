9e72ad38e06d6706cf6f001f423a7b35fd1d8fc3
khenpureza-cpu <khenpureza@gmail.com>
2025-11-21 00:12:15 +0800

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,10 @@
+# Activity 6: Lambda function
+add = lambda x, y: x + y
+multiply = lambda x, y: x * y
+square = lambda x: x ** 2
+num1 = float(input())
+num2 = float(input())
+
+print("Addition:", add(num1, num2))
+print("Multiplication:", multiply(num1, num2))
+print("Square of first:", square(num1))
