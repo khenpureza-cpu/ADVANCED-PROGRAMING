2a837500d02ab36382dc918abc15650b02767f19
khenpureza-cpu <khenpureza@gmail.com>
2025-11-20 23:46:58 +0800

    Create decissionfunc.py

---
+++ decissionfunc.py
@@ -0,0 +1,13 @@
+# Act 4: Decission function
+num1 = float(input())
+num2 = float(input())
+operator = input()
+
+if operator == '+':
+    print(num1 + num2)
+elif operator == '-':
+    print(num1 - num2)
+elif operator == '*':
+    print(num1 * num2)
+elif operator == '/':
+    print(num1 / num2)
