9b4bfc04f836e2a1ddf0371e2bb79be1f80da0bb
khenpureza-cpu <khenpureza@gmail.com>
2025-11-20 23:39:35 +0800

    Update and rename calcu.py to mathop.py

--- calcu.py
+++
@@ -1,8 +0,0 @@
-#activity 1- mathematical operator
-num1 = float(input())
-num2 = float(input())
-
-print( num1 + num2)
-print( num1 - num2)
-print(num1 * num2)
-print(num1 / num2)
---
+++ mathop.py
@@ -0,0 +1,5 @@
+#activity 1- mathematical operator
+print(5+5)
+print(10-5)
+print(5*5)
+print(10/5)
